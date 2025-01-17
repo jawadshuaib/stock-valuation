3d1fd9a4e660581fb5974ed74bd16052e16e70c6
Jawad <jawad.php@gmail.com>
2025-01-17 14:37:41 -0500

    Added API policies

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Homepage from './components/homepage/Homepage';
-import EPSCalculator from './components/calculators/eps/EPSCalculator'; // Ensure this is the correct path
+import EPSCalculator from './components/calculators/eps/EPSCalculator';
 import FCFCalculator from './components/calculators/fcf/FCFCalculator';
+import PrivacyPolicy from './components/privacy-policy/PrivacyPolicy';
 
 const AppRoutes: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const AppRoutes: React.FC = () => {
       <Route path="/" element={<Homepage />} />
       <Route path="/eps" element={<EPSCalculator />} />
       <Route path="/fcf" element={<FCFCalculator />} />
+      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
     </Routes>
   );
 };
