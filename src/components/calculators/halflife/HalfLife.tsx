291e156d099b855f0908347515edda2773540874
Jawad <jawad.php@gmail.com>
2025-01-20 05:30:31 -0500

    Added half life check

--- src/components/calculators/halflife/HalfLife.tsx
+++ src/components/calculators/halflife/HalfLife.tsx
@@ -7,7 +7,6 @@ interface HalfLifeProps {
 }
 
 export default function HalfLife({ data }: HalfLifeProps) {
-  console.log(data);
   const halfLife = HalfLifeCalculator.calculateHalfLife(
     data,
     (projection) => projection[data.method],
@@ -17,18 +16,25 @@ export default function HalfLife({ data }: HalfLifeProps) {
     data.method === 'eps'
       ? 'Price to Earnings Ratio'
       : 'Price to Free Cash Flow';
-  const abv = data.method === 'eps' ? 'P/E' : 'P/FCF';
+  // const abv = data.method === 'eps' ? 'P/E' : 'P/FCF';
+
+  if (halfLife === -1) return null;
 
   return (
     <section className="mt-8 p-6 bg-white rounded-lg border border-blue-100 shadow-sm">
       <h2 className="text-2xl font-semibold mb-4">Half Life</h2>
       <p className="text-lg mb-4">
-        Another valuation technique is to see how long it would take for the
-        company&apos;s {metric} to reach half of the current share price.
+        The Half Life valuation technique estimates the time it will take for
+        the company&apos;s {metric} to reduce to half of its current value. This
+        measure helps investors understand the rate at which a company&apos;s
+        valuation metrics are expected to decline, providing insight into the
+        company&apos;s future performance and potential risks.
       </p>
       <p className="text-lg">
-        The {abv} ratio will reach half of the current share price in
-        approximately {halfLife} years.
+        Companies with a half life of less than 10 years are considered to have
+        a high growth potential, while those with a half life of more than 10
+        years are considered to be mature companies with stable growth. This
+        investment has a half life of {halfLife} years. Which is great! ✅
       </p>
     </section>
   );
