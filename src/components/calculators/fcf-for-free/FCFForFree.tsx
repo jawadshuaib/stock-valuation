91e2744f12c660f848581c778c7f231c05591386
Jawad <jawad.php@gmail.com>
2025-01-20 07:34:32 -0500

    Working on FCFForFree

---
+++ src/components/calculators/fcf-for-free/FCFForFree.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function FCFForFree() {
+  return (
+    <div>
+      Free Cash Flow for Free measures how quickly the net present value of the
+      company&apos;s projected free cash flows (using a DCF and growth
+      assumptions) covers the difference between its market cap and net current
+      assets. In other words, it tells us how many years it takes before the
+      *premium* we pay beyond the company&apos;s near-liquid assets effectively
+      becomes free—fully paid back by the business&apos;s future free cash flow.
+    </div>
+  );
+}
