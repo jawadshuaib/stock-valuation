dde7bd9577ed4db214f57a5db226b0c21daa2b67
Jawad <jawad.php@gmail.com>
2025-01-19 21:38:11 -0500

    Edited txt

--- src/components/calculators/investment-growth/InvestmentGrowth.tsx
+++ src/components/calculators/investment-growth/InvestmentGrowth.tsx
@@ -23,8 +23,8 @@ export default function InvestmentGrowth({ data }: InvestmentGrowthProps) {
     <section className="mt-8 p-6 bg-white rounded-lg border border-blue-100 shadow-sm">
       <h2 className="text-2xl font-semibold mb-4">Investment Growth</h2>
       <p className="text-lg mb-4">
-        An initial Investment of ${initialInvestment.toLocaleString()} will have
-        grown to{' '}
+        At this growth rate, an initial Investment of $
+        {initialInvestment.toLocaleString()} will have grown to{' '}
         <span className="font-bold">
           ${Number(finalInvestment.toFixed(0)).toLocaleString()}
         </span>{' '}
