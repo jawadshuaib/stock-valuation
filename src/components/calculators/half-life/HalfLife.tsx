b74f1538ddc27220dcd4ecc731537130aa62dd6d
Jawad <jawad.php@gmail.com>
2025-01-20 05:41:20 -0500

    Improved explanation

--- src/components/calculators/half-life/HalfLife.tsx
+++ src/components/calculators/half-life/HalfLife.tsx
@@ -27,14 +27,15 @@ export default function HalfLife({ data }: HalfLifeProps) {
         The Half Life valuation technique estimates the time it will take for
         the company&apos;s {metric} to reduce to half of its current value. This
         measure helps investors understand the rate at which a company&apos;s
-        valuation metrics are expected to decline, providing insight into the
-        company&apos;s future performance and potential risks.
+        valuation metrics are improving, providing insight into the
+        company&apos;s future performance.
       </p>
       <p className="text-lg">
         Companies with a half life of less than 10 years are considered to have
-        a high growth potential, while those with a half life of more than 10
-        years are considered to be mature companies with stable growth. This
-        investment has a half life of {halfLife} years. Which is great! ✅
+        a high growth potential relative to price, while those with a half life
+        of more than 10 years are considered to be mature companies with stable
+        growth. This investment has a half life of {halfLife} years. Which is
+        great! ✅
       </p>
     </section>
   );
