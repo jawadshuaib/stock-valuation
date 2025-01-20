47981f70cb154fa42a928a49f015b9891d699d71
Jawad <jawad.php@gmail.com>
2025-01-20 05:55:25 -0500

    Improved explanation

--- src/components/calculators/half-life/HalfLife.tsx
+++ src/components/calculators/half-life/HalfLife.tsx
@@ -12,10 +12,10 @@ export default function HalfLife({ data }: HalfLifeProps) {
     (projection) => projection[data.method],
   );
 
-  const metric =
-    data.method === 'eps'
-      ? 'Price to Earnings Ratio'
-      : 'Price to Free Cash Flow';
+  // const metric =
+  //   data.method === 'eps'
+  //     ? 'Price to Earnings Ratio'
+  //     : 'Price to Free Cash Flow';
   // const abv = data.method === 'eps' ? 'P/E' : 'P/FCF';
 
   if (halfLife === -1) return null;
@@ -24,18 +24,20 @@ export default function HalfLife({ data }: HalfLifeProps) {
     <section className="mt-8 p-6 bg-white rounded-lg border border-blue-100 shadow-sm">
       <h2 className="text-2xl font-semibold mb-4">Half Life</h2>
       <p className="text-lg mb-4">
+        A company may be trading at high multiples today, but its growth
+        trajectory might justify the valuation. An investor can use time
+        arbitrage to their advantage by estimating how quickly this investment
+        becomes cheaper.
+      </p>
+      <p className="text-lg mb-4">
         The Half Life valuation technique estimates the time it will take for
-        the company&apos;s {metric} to reduce to half of its current value. This
-        measure helps investors understand the rate at which a company&apos;s
-        valuation is improving, providing insight into the company&apos;s future
-        performance.
+        the investment to become twice as cheap.
       </p>
       <p className="text-lg">
         Companies with a half life of less than 10 years are considered to have
-        a high growth potential relative to price, while those with a half life
-        of more than 10 years are considered to be mature companies with stable
-        growth. This investment has a half life of {halfLife} years. Which is
-        great! ✅
+        high growth potential relative to price. Overtime, we expect the market
+        to realize the company&apos;s true worth. This investment has a half
+        life of {halfLife} years, which is excellent! ✅
       </p>
     </section>
   );
