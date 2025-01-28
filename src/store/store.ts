bd425e8906b4873ba021a4422d3540c0502f2c4e
Jawad <jawad.php@gmail.com>
2025-01-28 06:42:46 -0500

    Added comments to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,14 @@
+/**
+ * @file store.ts
+ * @description This file sets up the Redux store for the stock valuation application using Redux Toolkit.
+ * It imports the necessary slices and configures the store with middleware settings.
+ */
 import { configureStore } from '@reduxjs/toolkit';
 import simulation from './slices/simulation';
 import loader from './slices/loader';
 
 // Determine if the current environment is a test environment
+// This flag is set automatically by Jest
 const isTest = process.env.NODE_ENV === 'test';
 
 const store = configureStore({
@@ -10,6 +16,9 @@ const store = configureStore({
     simulation,
     loader,
   },
+  // This code configures the Redux store's middleware to disable
+  // serializable and immutable checks when the application is
+  // running in a test environment
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: !isTest,
